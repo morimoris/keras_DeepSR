3b8f87e9b00c9b2182eb09e0ebf6011c81c645f3
moris <44795768+morimoris@users.noreply.github.com>
2021-04-23 22:26:37 +0900

    Delete psnr.py

--- psnr.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img_high = cv2.imread("result/model_b/high_b_38.jpg")
-img_low = cv2.imread("result/model_b/low_b_38.jpg")
-img_pred = cv2.imread("result/model_b/pred_b_38.jpg")
-
-print("low : " + str(cv2.PSNR(img_high, img_low)))
-print("pred : " + str(cv2.PSNR(img_high, img_pred)))
